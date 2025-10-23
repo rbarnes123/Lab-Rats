5b5289c9d02ff92aa617e026a299e7c0bed5e10f
rbarnes123 <rbarnes1@umbc.edu>
2025-10-23 16:39:25 -0400

    Upated Simon Puzzle
    Changed puzzle so it doesn't take player input while displaying pattern or showing player is wrong. Also changed it so that when a player clicks the wrong button they know immediately that it was wrong. Changed puzzle to be more forgiving. Now if player is wrong it resets to the start of the stage they got wrong instead of the start of the puzzle.

--- src/server/ratServer/simonPuzzle.server.lua
+++ src/server/ratServer/simonPuzzle.server.lua
@@ -22,45 +22,61 @@ local bodyTool = replicatedStorage:FindFirstChild("FrankeinRatBody")
 local numCorrect = 0
 local playerInput = ""
 local puzzle = ""
--- debounces for starting puzzle loop, grabbing body, lights taking input if puzzle incorrect
-local DB = false
+local puzzleIndex = 1
+local correctFlag = true
+-- allows one copy of the body to be picked up
 local touchDB = false
-local correctDB = false
+-- puzzle only takes input when pattern not displaying
+local inputDB = True
  
-local buttonflag = true
 -- toggles passed light on and off and plays the sound
-local function tgLight(light, buttonFlag)
+local function tgLight(light)
     light.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
     light.simonSound:Play()
-    buttonFlag = buttonFlag or false
     -- if script triggers light toggle, waits to switch
     task.wait(0.2)
     light.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
 end
 
+-- compares user input to answer in puzzle
+local function checkPuzzle(playerInput)
+    if puzzleIndex <= #puzzle then
+        if puzzle:sub(puzzleIndex,puzzleIndex) == playerInput then
+            puzzleIndex += 1
+        else
+            correctFlag = false
+            puzzleIndex = 1
+        end
+    end
+end
+
 -- gets button input
 button1.MouseClick:Connect(function()
-    if correctDB == false then
-        playerInput = playerInput.."1"
-        tgLight(light1,buttonFlag)
+    if inputDB == false then
+        playerInput = "1"
+        tgLight(light1)
+        checkPuzzle(playerInput)
     end
 end)
 button2.MouseClick:Connect(function()
-    if correctDB == false then
-        playerInput = playerInput.."2"
+    if inputDB == false then
+        playerInput = "2"
         tgLight(light2)
+        checkPuzzle(playerInput)
     end
 end)
 button3.MouseClick:Connect(function()
-    if correctDB == false then
-        playerInput = playerInput.."3"
+    if inputDB == false then
+        playerInput = "3"
         tgLight(light3)
+        checkPuzzle(playerInput)
     end
 end)
 button4.MouseClick:Connect(function()
-    if correctDB == false then
-        playerInput = playerInput.."4"
+    if inputDB == false then
+        playerInput = "4"
         tgLight(light4)
+        checkPuzzle(playerInput)
     end
 end)
 light3.simonSound.PlaybackRegionsEnabled = true
@@ -68,197 +84,218 @@ light3.simonSound.PlaybackRegion = NumberRange.new(0,0.5)
 
 local function simonHandler()
     -- main loop moves to next stage of the puzzle if previous was correct
-    if DB == false then
-        DB = true
-        while numCorrect < 10 do
-            playerInput = ""
-            -- handles which part of the puzzle the user is in
-            if numCorrect == 0 then
-                tgLight(light3)
-                puzzle = "3"
-            elseif numCorrect == 1 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                puzzle = "31"
-            elseif numCorrect == 2 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                puzzle = "311"
-            elseif numCorrect == 3 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                puzzle = "3112"
-            elseif numCorrect == 4 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light3)
-                puzzle = "31123"
-            elseif numCorrect == 5 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light4)
-                puzzle = "311234"
-            elseif numCorrect == 6 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light4)
-                task.wait(0.5)
-                tgLight(light3)
-                puzzle = "3112343"
-            elseif numCorrect == 7 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light4)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light2)
-                puzzle = "31123432"
-            elseif numCorrect == 8 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light4)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light4)
-                puzzle = "311234324"
-            elseif numCorrect == 9 then
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light1)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light4)
-                task.wait(0.5)
-                tgLight(light3)
-                task.wait(0.5)
-                tgLight(light2)
-                task.wait(0.5)
-                tgLight(light4)
-                task.wait(0.5)
-                tgLight(light1)
-                puzzle = "3112343241"
-            else
-                print("how")
-            end
-
-            while #playerInput < #puzzle do
-                task.wait(2)
-            end
-            if puzzle == playerInput then
-                numCorrect += 1
-            else
-                light1.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-                light2.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-                light3.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-                light4.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-                light1.errorSound.Playing = true
-                task.wait(1)
-                light1.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
-                light2.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
-                light3.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
-                light4.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
-                numCorrect = 0
-            end
-            task.wait(1)
+    while numCorrect < 10 do
+        playerInput = ""
+        -- handles which part of the puzzle the user is in
+        if numCorrect == 0 then
+            inputDB = true
+            tgLight(light3)
+            puzzle = "3"
+            inputDB = false
+        elseif numCorrect == 1 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            puzzle = "31"
+            inputDB = false
+        elseif numCorrect == 2 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            puzzle = "311"
+            inputDB = false
+        elseif numCorrect == 3 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            puzzle = "3112"
+            inputDB = false
+        elseif numCorrect == 4 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light3)
+            puzzle = "31123"
+            inputDB = false
+        elseif numCorrect == 5 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light4)
+            puzzle = "311234"
+            inputDB = false
+        elseif numCorrect == 6 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light4)
+            task.wait(0.5)
+            tgLight(light3)
+            puzzle = "3112343"
+            inputDB = false
+        elseif numCorrect == 7 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light4)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light2)
+            puzzle = "31123432"
+            inputDB = false
+        elseif numCorrect == 8 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light4)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light4)
+            puzzle = "311234324"
+            inputDB = false
+        elseif numCorrect == 9 then
+            inputDB = true
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light1)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light4)
+            task.wait(0.5)
+            tgLight(light3)
+            task.wait(0.5)
+            tgLight(light2)
+            task.wait(0.5)
+            tgLight(light4)
+            task.wait(0.5)
+            tgLight(light1)
+            puzzle = "3112343241"
+            inputDB = false
+        else
+            print("how")
         end
-        -- when puzzle is complete, all lights stay red, and the body drops from the ceiling
-        light1.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-        light2.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-        light3.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-        light4.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
-        correctDB = True
-        -- get body from storage and spawn it inside the pipe
-        local body = bodyTool:Clone()
-        body.Parent = game.Workspace
-        -- drop body and play sounds
-        body.ratBodyTool.hatch.Playing = true
-        body.ratBodyTool.Anchored = true
+        -- change loop below to have it wait until either the player is wrong or the length matches 
+        while correctFlag == true and puzzleIndex <= #puzzle do
+            task.wait(2)
+        end
+        -- if puzzle correct, add a light to the pattern and reset 
+        if correctFlag == true then
+            numCorrect += 1
+            puzzleIndex = 1
+        else
+            inputDB = true
+            light1.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+            light2.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+            light3.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+            light4.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+            light1.errorSound.Playing = true
+            task.wait(1)
+            light1.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
+            light2.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
+            light3.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
+            light4.Glass_LIGHT.BrickColor = BrickColor.new("Really black")
+            -- reset correct flag
+            correctFlag = true
+        end
         task.wait(1)
-        body.ratBodyTool.Anchored = false
-        task.wait(.25)
-        body.ratBodyTool.splat.Playing = true
-        body.ratBodyTool.glow.Enabled = true
-        -- add body to all player's backpacks when touched
-        task.wait(3)
-        body.ratBodyTool.Touched:Connect(function()
-            if touchDB == false then 
-                touchDB = true
-                for _, player in players:GetPlayers() do
-                    -- adds the body to the player, 
-                    if player:FindFirstChild("Backpack") then
-                        local playerTool = bodyTool:Clone()
-                        playerTool.Parent = player.Backpack
-                        -- hides body copies
-                        playerTool.ratBodyTool.Transparency = 1
-                        playerTool.ratBodyTool.LeftArm.Transparency = 1
-                        playerTool.ratBodyTool.RightArm.Transparency = 1
-                        playerTool.ratBodyTool.legs.Transparency = 1
-                        playerTool.ratBodyTool.legs.RightLeg.Transparency = 1
-                        playerTool.ratBodyTool.CanCollide = false
-                        playerTool.ratBodyTool.Anchored = true
-                    end
-                end
-                -- deletes the body upon player pickup
-                body:Destroy()
-            end
-        end)
     end
+    -- when puzzle is complete, all lights stay red, and the body drops from the ceiling
+    light1.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+    light2.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+    light3.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+    light4.Glass_LIGHT.BrickColor = BrickColor.new("Persimmon")
+    inputDB = true
+    -- get body from storage and spawn it inside the pipe
+    local body = bodyTool:Clone()
+    body.Parent = game.Workspace
+    -- drop body and play sounds
+    body.ratBodyTool.hatch.Playing = true
+    body.ratBodyTool.Anchored = true
+    task.wait(1)
+    body.ratBodyTool.Anchored = false
+    task.wait(.25)
+    body.ratBodyTool.splat.Playing = true
+    body.ratBodyTool.glow.Enabled = true
+    -- add body to all player's backpacks when touched
+    task.wait(3)
+    body.ratBodyTool.Touched:Connect(function()
+        if touchDB == false then 
+            touchDB = true
+            for _, player in players:GetPlayers() do
+                -- adds the body to the player, 
+                if player:FindFirstChild("Backpack") then
+                    local playerTool = bodyTool:Clone()
+                    playerTool.Parent = player.Backpack
+                    -- hides body copies
+                    playerTool.ratBodyTool.Transparency = 1
+                    playerTool.ratBodyTool.LeftArm.Transparency = 1
+                    playerTool.ratBodyTool.RightArm.Transparency = 1
+                    playerTool.ratBodyTool.legs.Transparency = 1
+                    playerTool.ratBodyTool.legs.RightLeg.Transparency = 1
+                    playerTool.ratBodyTool.CanCollide = false
+                    playerTool.ratBodyTool.Anchored = true
+                end
+            end
+            -- deletes the body upon player pickup
+            body:Destroy()
+        end
+    end)
 end
 -- starts the puzzle
 startSwitch.Triggered:Connect(function()
